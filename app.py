39d9280380ff5b862fa20b4236028b5c9fd118e0
JasonHongGG <p76134040@gs.ncku.edu.tw>
2025-09-27 00:55:33 +0800

    fix: host ip

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ class App:
         
         # 啟動 Flask 服務器
         cameraServer = CameraServer(self.processor)
-        cameraServer.app.run(host='127.0.0.1', port=5000, debug=False, threaded=True)
+        cameraServer.app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
 
 if __name__ == "__main__":
     # 載入環境變數
